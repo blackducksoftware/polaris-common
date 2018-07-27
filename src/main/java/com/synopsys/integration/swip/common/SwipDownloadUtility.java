2967212ff230cc8e6ff8ad9b2d08c58de79ca082
Eric Kerwin <ekerwin@kingofnerds.com>
2018-07-27 08:48:01 -0400

    final changes from code review

--- src/main/java/com/synopsys/integration/swip/common/SwipDownloadUtility.java
+++ src/main/java/com/synopsys/integration/swip/common/SwipDownloadUtility.java
@@ -24,27 +24,23 @@
 package com.synopsys.integration.swip.common;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Optional;
 
 import org.apache.commons.compress.archivers.ArchiveException;
-import org.apache.commons.compress.archivers.examples.Expander;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 
 import com.blackducksoftware.integration.exception.IntegrationException;
 import com.blackducksoftware.integration.log.IntLogger;
-import com.blackducksoftware.integration.rest.HttpMethod;
+import com.blackducksoftware.integration.rest.connection.RestConnection;
 import com.blackducksoftware.integration.rest.connection.UnauthenticatedRestConnection;
 import com.blackducksoftware.integration.rest.proxy.ProxyInfo;
 import com.blackducksoftware.integration.rest.request.Request;
 import com.blackducksoftware.integration.rest.request.Response;
+import com.blackducksoftware.integration.util.CleanupZipExpander;
 
 public class SwipDownloadUtility {
     public static final String DEFAULT_SWIP_SERVER_URL = "https://tools.swip.synopsys.com";
@@ -57,20 +53,25 @@ public class SwipDownloadUtility {
     public static final String VERSION_FILENAME = "swipVersion.txt";
 
     private final IntLogger logger;
+    private final RestConnection restConnection;
+    private final CleanupZipExpander cleanupZipExpander;
     private final String swipServerUrl;
     private final File installDirectory;
 
-    /**
-     * Given a valid Swip url and a valid download directory, you can
-     * getSwipCliExecutablePath() which will, if necessary, download the Swip
-     * CLI from the server and provide you the correct path to the executable.
-     */
-    public SwipDownloadUtility(IntLogger logger, String swipServerUrl, File downloadTargetDirectory) {
+    public static SwipDownloadUtility defaultUtility(IntLogger logger, File downloadTargetDirectory) {
+        UnauthenticatedRestConnection restConnection = new UnauthenticatedRestConnection(logger, null, ProxyInfo.NO_PROXY_INFO);
+        CleanupZipExpander cleanupZipExpander = new CleanupZipExpander(logger);
+        return new SwipDownloadUtility(logger, restConnection, cleanupZipExpander, DEFAULT_SWIP_SERVER_URL, downloadTargetDirectory);
+    }
+
+    public SwipDownloadUtility(IntLogger logger, RestConnection restConnection, CleanupZipExpander cleanupZipExpander, String swipServerUrl, File downloadTargetDirectory) {
         if (StringUtils.isBlank(swipServerUrl)) {
             throw new IllegalArgumentException("A Swip server url must be provided.");
         }
 
         this.logger = logger;
+        this.restConnection = restConnection;
+        this.cleanupZipExpander = cleanupZipExpander;
         this.swipServerUrl = swipServerUrl;
         installDirectory = new File(downloadTargetDirectory, SWIP_CLI_INSTALL_DIRECTORY);
 
@@ -87,23 +88,23 @@ public class SwipDownloadUtility {
      * otherwise an Optional.empty will be returned and the log will contain
      * details concerning the failure.
      */
-    public Optional<String> getSwipCliExecutablePath() {
+    public Optional<String> retrieveSwipCliExecutablePath() {
         File versionFile = null;
         try {
-            versionFile = getVersionFile();
+            versionFile = retrieveVersionFile();
         } catch (IOException e) {
             logger.error("Could not create the version file: " + e.getMessage());
             return Optional.empty();
         }
 
         String downloadUrl = getDownloadUrl();
-        return getSwipCliExecutablePath(versionFile, downloadUrl);
+        return retrieveSwipCliExecutablePath(versionFile, downloadUrl);
     }
 
-    public Optional<String> getSwipCliExecutablePath(File versionFile, String downloadUrl) {
+    public Optional<String> retrieveSwipCliExecutablePath(File versionFile, String downloadUrl) {
         File binDirectory = null;
         try {
-            binDirectory = downloadIfNotModified(versionFile, downloadUrl);
+            binDirectory = downloadIfModified(versionFile, downloadUrl);
         } catch (Exception e) {
             logger.error("The Swip CLI could not be downloaded successfully: " + e.getMessage());
         }
@@ -121,7 +122,7 @@ public class SwipDownloadUtility {
         return Optional.empty();
     }
 
-    public File getVersionFile() throws IOException {
+    public File retrieveVersionFile() throws IOException {
         File versionFile = new File(installDirectory, VERSION_FILENAME);
         if (!versionFile.exists()) {
             logger.info("The version file has not been created yet so creating it now.");
@@ -142,78 +143,42 @@ public class SwipDownloadUtility {
         }
     }
 
-    private File downloadIfNotModified(File versionFile, String downloadUrl) throws IOException, IntegrationException, ArchiveException {
+    private File downloadIfModified(File versionFile, String downloadUrl) throws IOException, IntegrationException, ArchiveException {
         long lastTimeDownloaded = versionFile.lastModified();
         logger.debug(String.format("last time downloaded: %d", lastTimeDownloaded));
 
-        Request.Builder requestBuilder = new Request.Builder(downloadUrl).method(HttpMethod.HEAD);
-        Request headRequest = requestBuilder.build();
+        Request downloadRequest = new Request.Builder(downloadUrl).build();
+        Optional<Response> optionalResponse = restConnection.executeGetRequestIfModifiedSince(downloadRequest, lastTimeDownloaded);
+        if (optionalResponse.isPresent()) {
+            Response response = optionalResponse.get();
+            try {
+                logger.info("Downloading the Swip CLI.");
+                try (InputStream responseStream = response.getContent()) {
+                    cleanupZipExpander.expand(responseStream, installDirectory);
+                }
+                long lastModifiedOnServer = response.getLastModified();
+                versionFile.setLastModified(lastModifiedOnServer);
 
-        requestBuilder.method(HttpMethod.GET);
-        Request request = requestBuilder.build();
+                File binDirectory = getBinDirectory();
+                makeBinFilesExecutable(binDirectory);
 
-        UnauthenticatedRestConnection unauthenticatedRestConnection = new UnauthenticatedRestConnection(logger, null, 120, ProxyInfo.NO_PROXY_INFO);
+                logger.info(String.format("Swip CLI downloaded successfully."));
 
-        long lastModifiedOnServer = 0L;
-        try (Response headResponse = unauthenticatedRestConnection.executeRequest(headRequest)) {
-            lastModifiedOnServer = headResponse.getLastModified();
-            logger.debug(String.format("Last modified on server: %d", lastModifiedOnServer));
-        } catch (IntegrationException e) {
-            logger.error("Couldn't get the Last-Modified header from the server.");
-            throw e;
-        }
-
-        if (lastModifiedOnServer == lastTimeDownloaded) {
+                return binDirectory;
+            } finally {
+                response.close();
+            }
+        } else {
             logger.debug("The Swip CLI has not been modified since it was last downloaded - skipping download.");
-            String swipCliDirectoryName = FileUtils.readFileToString(versionFile, StandardCharsets.UTF_8);
             return getBinDirectory();
         }
-
-        // it is important to first create the zip file as a stream cannot be
-        // unzipped correctly in all cases
-        // "If possible, you should always prefer ZipFile over
-        // ZipArchiveInputStream."
-        // https://commons.apache.org/proper/commons-compress/zip.html#ZipArchiveInputStream_vs_ZipFile
-        File tempZipFile = File.createTempFile("tmpzip", null);
-        try (Response response = unauthenticatedRestConnection.executeRequest(request)) {
-            logger.info("Downloading the Swip CLI.");
-
-            try (InputStream responseStream = response.getContent(); FileOutputStream fileOutputStream = new FileOutputStream(tempZipFile)) {
-                IOUtils.copy(responseStream, fileOutputStream);
-            }
-        } catch (IntegrationException e) {
-            throw e;
-        }
-
-        if (!tempZipFile.exists() || tempZipFile.length() <= 0) {
-            throw new IntegrationException("The zip file was not downloaded correctly. Please try again.");
-        }
-
-        Expander expander = new Expander();
-        try {
-            expander.expand(tempZipFile, installDirectory);
-        } catch (IOException | ArchiveException e) {
-            logger.error("Couldn't extract the zip file - check the file's permissions: " + e.getMessage());
-            throw e;
-        }
-
-        FileUtils.deleteQuietly(tempZipFile);
-
-        versionFile.setLastModified(lastModifiedOnServer);
-
-        File binDirectory = getBinDirectory();
-        makeBinFilesExecutable(binDirectory);
-
-        logger.info(String.format("Swip CLI downloaded successfully."));
-
-        return binDirectory;
     }
 
     // since we know that we only allow a single directory in installDirectory,
-    // that single directory IS the extracted archive
+    // that single directory IS the expanded archive
     private File getBinDirectory() throws IntegrationException {
         File[] directories = installDirectory.listFiles(file -> file.isDirectory());
-        if (directories.length != 1) {
+        if (directories == null || directories.length != 1) {
             throw new IntegrationException(String.format("The %s directory should only be modified by swip-common. Please delete all files from that directory and try again.", SWIP_CLI_INSTALL_DIRECTORY));
         }
 
--- src/test/java/com/synopsys/integration/swip/common/SwipDownloadUtilityTest.java
+++ src/test/java/com/synopsys/integration/swip/common/SwipDownloadUtilityTest.java
@@ -1,25 +1,116 @@
 package com.synopsys.integration.swip.common;
 
 import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import com.blackducksoftware.integration.log.IntLogger;
+import com.blackducksoftware.integration.rest.connection.RestConnection;
+import com.blackducksoftware.integration.rest.request.Request;
+import com.blackducksoftware.integration.rest.request.Response;
 import com.blackducksoftware.integration.test.tool.TestLogger;
+import com.blackducksoftware.integration.util.CleanupZipExpander;
 
 public class SwipDownloadUtilityTest {
     @Test
-    public void testAttemptingDownload() throws Exception {
-        IntLogger intLogger = new TestLogger();
-        File downloadTarget = new File("/Users/ekerwin/working/swip");
+    @Ignore
+    // ekerwin 2018-07-23 leaving this test as it can be useful in diagnosing
+    // problems to actually perform the download, but it shouldn't run normally
+    // in dev/ci environments.
+    public void testActualDownload() {
+        String swipCLIDownloadPath = System.getenv("SWIP_CLI_DOWNLOAD_PATH");
+        File downloadTarget = new File(swipCLIDownloadPath);
 
-        SwipDownloadUtility swipDownloadUtility = new SwipDownloadUtility(intLogger, SwipDownloadUtility.DEFAULT_SWIP_SERVER_URL, downloadTarget);
-        Optional<String> swipCliPath = swipDownloadUtility.getSwipCliExecutablePath();
+        IntLogger intLogger = new TestLogger();
+        SwipDownloadUtility swipDownloadUtility = SwipDownloadUtility.defaultUtility(intLogger, downloadTarget);
+
+        Optional<String> swipCliPath = swipDownloadUtility.retrieveSwipCliExecutablePath();
+        Assert.assertTrue(swipCliPath.isPresent());
+        Assert.assertTrue(swipCliPath.get().length() > 0);
+    }
+
+    @Test
+    public void testInitialDownload() throws Exception {
+        InputStream zipFileStream = getClass().getResourceAsStream("/swip_mac.zip");
+        Response mockResponse = Mockito.mock(Response.class);
+        Mockito.when(mockResponse.getContent()).thenReturn(zipFileStream);
+
+        RestConnection mockRestConnection = Mockito.mock(RestConnection.class);
+        Mockito.when(mockRestConnection.executeGetRequestIfModifiedSince(Mockito.any(Request.class), Mockito.anyLong())).thenReturn(Optional.of(mockResponse));
+
+        IntLogger intLogger = new TestLogger();
+        Path tempDirectory = Files.createTempDirectory(null);
+        File downloadTarget = tempDirectory.toFile();
+        downloadTarget.deleteOnExit();
+
+        CleanupZipExpander cleanupZipExpander = new CleanupZipExpander(intLogger);
+        SwipDownloadUtility swipDownloadUtility = new SwipDownloadUtility(intLogger, mockRestConnection, cleanupZipExpander, SwipDownloadUtility.DEFAULT_SWIP_SERVER_URL, downloadTarget);
+        Optional<String> swipCliPath = swipDownloadUtility.retrieveSwipCliExecutablePath();
 
         Assert.assertTrue(swipCliPath.isPresent());
         Assert.assertTrue(swipCliPath.get().length() > 0);
+    }
+
+    @Test
+    public void testNotDownloadIfNotUpdatedOnServer() throws Exception {
+        RestConnection mockRestConnection = Mockito.mock(RestConnection.class);
+        Mockito.when(mockRestConnection.executeGetRequestIfModifiedSince(Mockito.any(Request.class), Mockito.anyLong())).thenReturn(Optional.empty());
+
+        TestLogger intLogger = new TestLogger();
+
+        Path tempDirectory = Files.createTempDirectory(null);
+        File downloadTarget = tempDirectory.toFile();
+        downloadTarget.deleteOnExit();
+
+        CleanupZipExpander cleanupZipExpander = new CleanupZipExpander(intLogger);
+        SwipDownloadUtility swipDownloadUtility = new SwipDownloadUtility(intLogger, mockRestConnection, cleanupZipExpander, SwipDownloadUtility.DEFAULT_SWIP_SERVER_URL, downloadTarget);
+        Optional<String> swipCliPath = swipDownloadUtility.retrieveSwipCliExecutablePath();
+
+        Assert.assertFalse(swipCliPath.isPresent());
+        Assert.assertTrue(intLogger.getOutputString().contains("skipping download"));
+    }
+
+    @Test
+    public void testDownloadIfServerUpdated() throws Exception {
+        InputStream zipFileStream = getClass().getResourceAsStream("/swip_mac.zip");
+        Response mockResponse = Mockito.mock(Response.class);
+        Mockito.when(mockResponse.getContent()).thenReturn(zipFileStream);
+
+        RestConnection mockRestConnection = Mockito.mock(RestConnection.class);
+        Mockito.when(mockRestConnection.executeGetRequestIfModifiedSince(Mockito.any(Request.class), Mockito.anyLong())).thenReturn(Optional.of(mockResponse));
+
+        TestLogger intLogger = new TestLogger();
+
+        Path tempDirectory = Files.createTempDirectory(null);
+        File downloadTarget = tempDirectory.toFile();
+        downloadTarget.deleteOnExit();
+
+        File installDirectory = new File(downloadTarget, SwipDownloadUtility.SWIP_CLI_INSTALL_DIRECTORY);
+        installDirectory.mkdirs();
+        installDirectory.deleteOnExit();
+
+        // create a directory that should be deleted by the update download/extract code
+        File directoryOfPreviousExtraction = new File(installDirectory, "temp_swip_cli_version");
+        directoryOfPreviousExtraction.mkdirs();
+        Assert.assertTrue(directoryOfPreviousExtraction.isDirectory());
+        Assert.assertTrue(directoryOfPreviousExtraction.exists());
+
+        CleanupZipExpander cleanupZipExpander = new CleanupZipExpander(intLogger);
+        SwipDownloadUtility swipDownloadUtility = new SwipDownloadUtility(intLogger, mockRestConnection, cleanupZipExpander, SwipDownloadUtility.DEFAULT_SWIP_SERVER_URL, downloadTarget);
+        Optional<String> swipCliPath = swipDownloadUtility.retrieveSwipCliExecutablePath();
+
+        Assert.assertTrue(swipCliPath.isPresent());
+        Assert.assertTrue(swipCliPath.get().length() > 0);
+        Assert.assertFalse(directoryOfPreviousExtraction.exists());
+        Assert.assertTrue(intLogger.getOutputString().contains("There were items"));
+        Assert.assertTrue(intLogger.getOutputString().contains("that are being deleted"));
     }
 
 }

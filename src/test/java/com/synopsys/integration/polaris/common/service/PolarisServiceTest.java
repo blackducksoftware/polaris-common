7f630ecf685544f3ff31a6baa161bcdd473e6738
Gavin Killough <gavink@synopsys.com>
2019-01-22 09:14:57 -0500

    Create PolarisService and add tests

--- src/test/java/com/synopsys/integration/polaris/common/service/PolarisServiceTest.java
+++ src/test/java/com/synopsys/integration/polaris/common/service/PolarisServiceTest.java
@@ -1,6 +1,8 @@
-package com.synopsys.integration.polaris.common;
+package com.synopsys.integration.polaris.common.service;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,19 +11,26 @@ import com.synopsys.integration.exception.IntegrationException;
 import com.synopsys.integration.log.IntLogger;
 import com.synopsys.integration.log.LogLevel;
 import com.synopsys.integration.log.PrintStreamIntLogger;
+import com.synopsys.integration.polaris.common.api.BranchV0;
 import com.synopsys.integration.polaris.common.api.BranchV0Resources;
+import com.synopsys.integration.polaris.common.api.ResourcesPagination;
 import com.synopsys.integration.polaris.common.rest.AccessTokenPolarisHttpClient;
 import com.synopsys.integration.polaris.common.rest.AccessTokenPolarisHttpClientTestIT;
-import com.synopsys.integration.rest.HttpMethod;
 import com.synopsys.integration.rest.proxy.ProxyInfo;
 import com.synopsys.integration.rest.request.Request;
-import com.synopsys.integration.rest.request.Response;
 import com.synopsys.integration.rest.support.AuthenticationSupport;
 
-public class CookbookTestIT {
+public class PolarisServiceTest {
 
     @Test
-    public void getSomePolarisProjectsTest() throws IntegrationException, IOException {
+    public void createDefaultPolarisGetRequestTest() {
+        final PolarisService polarisService = new PolarisService(null, null, null);
+        final Request request = polarisService.createDefaultPolarisGetRequest("https://google.com");
+        assertNotNull(request);
+    }
+
+    @Test
+    public void executeGetRequestTestIT() throws IntegrationException {
         final String baseUrl = System.getenv(AccessTokenPolarisHttpClientTestIT.ENV_POLARIS_BASE_URL);
         final String accessToken = System.getenv(AccessTokenPolarisHttpClientTestIT.ENV_POLARIS_ACCESS_TOKEN);
 
@@ -29,15 +38,15 @@ public class CookbookTestIT {
         final IntLogger logger = new PrintStreamIntLogger(System.out, LogLevel.INFO);
         final AccessTokenPolarisHttpClient httpClient = new AccessTokenPolarisHttpClient(logger, 100, true, ProxyInfo.NO_PROXY_INFO, baseUrl, accessToken, gson, new AuthenticationSupport());
 
-        final String requestUri = baseUrl + "/api/common/v0/branches";
-        final Request request = new Request.Builder().uri(requestUri).addQueryParameter("page[offset]", "0").addQueryParameter("page[limit]", "10").mimeType("application/vnd.api+json").method(HttpMethod.GET).build();
-        try (final Response response = httpClient.execute(request)) {
-            logger.info("Response status: " + response.getStatusCode());
-            logger.info("Response message: " + response.getStatusMessage());
+        final PolarisService polarisService = new PolarisService(logger, httpClient, gson);
 
-            final String contentString = response.getContentString();
-            final BranchV0Resources branches = gson.fromJson(contentString, BranchV0Resources.class);
-            logger.info("Response content: \n" + branches.toString());
-        }
+        final String requestUri = baseUrl + "/api/common/v0/branches";
+        final Request request = polarisService.createDefaultPolarisGetRequest(requestUri);
+
+        final BranchV0Resources branchV0Resources = polarisService.executeGetRequest(request, BranchV0Resources.class);
+        final List<BranchV0> branchV0ResourceList = branchV0Resources.getData();
+        assertNotNull(branchV0ResourceList);
+        final ResourcesPagination meta = branchV0Resources.getMeta();
+        assertNotNull(meta);
     }
 }
